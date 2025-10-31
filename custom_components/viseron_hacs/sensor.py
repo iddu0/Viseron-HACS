92cc29a656fd2a838874ee536afd71406b5d8bec
iddu01 <157660303+iddu0@users.noreply.github.com>
2025-10-30 20:59:34 -0400

    Add ExampleSensor to Home Assistant integration

--- custom_components/viseron_hacs/sensor.py
+++ custom_components/viseron_hacs/sensor.py
@@ -1,1 +1,19 @@
 # for testing, i'm learning!
+from homeassistant.helpers.entity import Entity
+
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    async_add_entities([ExampleSensor])
+
+class ExampleSensor(Entity):
+    def __init__(self):
+        self.__attr_name = "Example Sensor"
+        self.__attr_native_value = 42
+
+    @property
+    def name(self):
+        return self.__attr_name
+    
+    @property
+
+    def native_value(self):
+        return self.__attr_native_value
